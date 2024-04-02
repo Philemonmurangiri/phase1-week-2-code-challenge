7afe68a00036a6872288cf54c30c8e96094d4435
philemonmurangiri <murangirilesx890@gmail.com>
2024-04-02 12:47:12 +0300

    creates program in js

--- program3.js
+++ program3.js
@@ -27,4 +27,5 @@ function isPrime(num) {
     return primeNumbers;
   }
   
-  console.log(filterPrimeNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,13,17,19]));
+  console.log(filterPrimeNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,13,17,19,21
+  ]));
